d91c85d97ce3892fb43f0d62c2fde4cb8afc2bd7
Xicheng Xiong <xxiong@L-MBP15-03.local>
2020-06-09 23:37:30 -0700

    Updates

---
+++ precache-manifest.c677fb31326aefedf5d6488c3bf9e44d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3faf5cdf77c657b53d25da3da48a7a6a",
+    "url": "/porfolio/index.html"
+  },
+  {
+    "revision": "0299d270477904406f88",
+    "url": "/porfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e664689b215b3e4ea464",
+    "url": "/porfolio/static/js/2.70ff550c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/porfolio/static/js/2.70ff550c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0299d270477904406f88",
+    "url": "/porfolio/static/js/main.8d037d66.chunk.js"
+  },
+  {
+    "revision": "5176a1f56bd34026550b",
+    "url": "/porfolio/static/js/runtime-main.719ba060.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/porfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
